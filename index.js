ed089001dfeb22f2a2e68d7daa6f3a4c8f720558
MaxDobisz <maciejdobisz@gmail.com>
2022-11-26 17:14:52 +0000

    js refactor

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const nav = document.querySelector('.header__nav');
 const hamburgerClosed = document.querySelector('.header__hamburger--closed');
 const hamburgerOpen = document.querySelector('.header__hamburger--open');
@@ -6,35 +5,14 @@ const darkerBackgroundLayer = document.querySelector('.darker-background-layer')
 
 const toggleClass = (el, className) => el.classList.toggle(className);
 
-hamburgerClosed.addEventListener('click', () => {
-    toggleClass(); 
-    darkerBackgroundLayer.classList.add('darker-background-layer--active');
-});
+const toggleMenu = () => {
+    toggleClass(nav, 'header__nav--active');
+    toggleClass(darkerBackgroundLayer, 'darker-background-layer--active');
+}
 
-hamburgerOpen.addEventListener('click', () => {
-    toggleHamburgerMenu();
-    darkerBackgroundLayer.classList.remove('darker-background-layer--active');
-});
+hamburgerClosed.addEventListener('click', toggleMenu);
+hamburgerOpen.addEventListener('click', toggleMenu);
 
 const navItems = document.querySelectorAll('.nav__item');
-navItems.forEach(item => item.addEventListener('click', () => {
-    nav.classList.remove('header__nav--active');
-    darkerBackgroundLayer.classList.remove('darker-background-layer--active');
-}))
 
-
-// hamburgerClosed.addEventListener('click', () => {
-//     navigation.classList.add('header__nav--active');
-//     darkerBackgroundLayer.classList.add('darker-background-layer--active');
-// });
-
-// hamburgerOpen.addEventListener('click', () => {
-//     navigation.classList.remove('header__nav--active');
-//     darkerBackgroundLayer.classList.remove('darker-background-layer--active');
-// });
-
-// const navItems = document.querySelectorAll('.nav__item');
-// navItems.forEach(item => item.addEventListener('click', () => {
-//     navigation.classList.remove('header__nav--active');
-//     darkerBackgroundLayer.classList.remove('darker-background-layer--active');
-// }))
+navItems.forEach(item => item.addEventListener('click', toggleMenu));
