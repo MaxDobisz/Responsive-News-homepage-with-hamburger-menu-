21c3cc90778d101a53b037a31dcf6a62b050918b
MaxDobisz <maciejdobisz@gmail.com>
2022-11-27 12:02:48 +0000

    menu close on header issue fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('.header__nav');
-const hamburgerClosed = document.querySelector('.header__hamburger--closed');
-const hamburgerOpen = document.querySelector('.header__hamburger--open');
+const hamburgerClosed = document.querySelector('.header__hamburger-closed');
+const hamburgerOpen = document.querySelector('.header__hamburger-open');
 const darkerBackgroundLayer = document.querySelector('.darker-background-layer');
 
 const toggleClass = (el, className) => el.classList.toggle(className);
@@ -8,11 +8,11 @@ const toggleClass = (el, className) => el.classList.toggle(className);
 const toggleMenu = () => {
     toggleClass(nav, 'header__nav--active');
     toggleClass(darkerBackgroundLayer, 'darker-background-layer--active');
+    toggleClass(hamburgerOpen, 'header__hamburger-open--active')
+    console.log('clcik')
 }
 
 hamburgerClosed.addEventListener('click', toggleMenu);
 hamburgerOpen.addEventListener('click', toggleMenu);
-
-const navItems = document.querySelectorAll('.nav__item');
-
-navItems.forEach(item => item.addEventListener('click', toggleMenu));
+nav.addEventListener('click', toggleMenu);
+darkerBackgroundLayer.addEventListener('click', toggleMenu)
